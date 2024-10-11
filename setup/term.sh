9f99344668d8be58c04d2eacb3bbaf55ff6c8d2e
LisaJacklin <lmjacklin@alaska.edu>
2024-10-11 12:14:00 -0800

    Adding terminal packages to term

--- setup/term.sh
+++ setup/term.sh
@@ -2,7 +2,12 @@
 #
 
 #first installing some color
-echo "need to get some color in here first!"
+echo "#####################################"
+echo "     Setting Up Terminal for Use     "
+echo "#####################################"
+
+echo "first I need to get some color here...."
+
 if dpkg -l | grep lolcat > /dev/null; then
 	echo "Looks like you already have color! Great :)" | lolcat
 else
@@ -10,8 +15,29 @@ else
 	sudo apt install lolcat
 fi
 
-echo "Beginning Terminal Setup...." | lolcat
+echo "Now, let's get setup!" | lolcat
+echo "---------------------------------------"
 
+#install bash
+if dpkg -l | grep bash > /dev/null; then
+	echo "bash already installed" | lolcat
+else
+	echo "Installing bash..."
+	sudo apt install bash
+fi
+
+#install glow
+if dpkg -l | grep glow > /dev/null; then
+	echo "glow already installed" | lolcat
+else
+	echo "Installing glow..." | lolcat
+	sudo apt install glow
+	sudo snap install glow #if apt doesn't work...
+fi
+
+#need to install neovim/vim for editing
+
+#installing tmux
 if dpkg -l | grep tmux > /dev/null; then
 	echo "tmux is already installed" | lolcat
 else
@@ -20,13 +46,34 @@ else
 	sudo apt install tmuxinator
 fi
 #might as well add tmuxinator too...
-
+#adding git
 if dpkg -l | grep git > /dev/null; then
 	echo "git is already installed" | lolcat
 else
 	echo "Installing git..." | lolcat
 	sudo apt install git
 fi
+
+#adding telnet
+if dpkg -l | grep telnet > /dev/null; then
+	echo "telnet is already installed" | lolcat
+else
+	echo "Installing telnet" | lolcat
+	sudo apt install telnet
+fi
+
+#installing nmap
+if dpkg -l | grep nmap > /dev/null; then
+	echo "nmap is already installed" | lolcat
+else
+	echo "Installing nmap..." | lolcat
+	sudo apt install nmap
+fi
+
+
+echo "######################################" 
+echo "    Basic Terminal Setup Complete     " | lolcat
+echo "######################################" 
 
 
 
