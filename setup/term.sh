e8cb5772f46a15a7f9201aa11990144718c86a96
LisaJacklin <lmjacklin@alaska.edu>
2024-10-11 11:57:00 -0800

    simplify terminal output for term setup

--- setup/term.sh
+++ setup/term.sh
@@ -1,21 +1,30 @@
 #need to check and see what I have before I can go through and begin installing things that can be used to customize my terminal experience!
 #
 
-echo "Beginning Terminal Setup...."
-
-if dpkg -l | grep tmux; then
-	echo "tmux is already installed"
+#first installing some color
+echo "need to get some color in here first!"
+if dpkg -l | grep lolcat > /dev/null; then
+	echo "Looks like you already have color! Great :)" | lolcat
 else
-	echo "Installing tmux..."
-	sudo apt install tmux
+	echo " Lolcat is coloring..."
+	sudo apt install lolcat
+fi
+
+echo "Beginning Terminal Setup...." | lolcat
+
+if dpkg -l | grep tmux > /dev/null; then
+	echo "tmux is already installed" | lolcat
+else
+	echo "Installing tmux..." | lolcat
+	sudo apt install tmux 
 	sudo apt install tmuxinator
 fi
 #might as well add tmuxinator too...
 
-if dpkg -l | grep git; then
-	echo "git is already installed"
+if dpkg -l | grep git > /dev/null; then
+	echo "git is already installed" | lolcat
 else
-	echo "Installing git..."
+	echo "Installing git..." | lolcat
 	sudo apt install git
 fi
 
