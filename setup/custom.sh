126eaff8c6375164859e0e9d7f2d631d20e031c9
LisaJacklin <lmjacklin@alaska.edu>
2024-10-14 11:01:16 -0800

    fun packages added to terminal

---
+++ setup/custom.sh
@@ -0,0 +1,17 @@
+# time to get some fun things in here....
+
+echo "Time to get..." | lolcat
+echo "   CUSTOM     " | lolcat
+
+echo "Downloading something fun..."
+sudo apt install cmatrix
+sudo apt install libaa-bin
+sudo apt install cowsay
+sudo apt install oneko
+sudo apt install espeak
+
+
+echo "Now say hi to Jerry!"
+oneko
+
+
