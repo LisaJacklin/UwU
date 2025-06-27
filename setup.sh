a779888c721afa6d60a59726215cdbba649dc6db
LisaJacklin <lmjacklin@alaska.edu>
2025-06-27 14:26:07 -0800

    package list to setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+###################################
+# Author: Lisa Jacklin
+# Last Updated: 2025-06-27
+# Purpose: provide quick, easy install for a brand new system
+###################################
+
+#list of packages to install
+packages = (lolcat bash tmux telnet tree nmap netcat curl git vim htop wget ruby-full chruby asciinema)
+
+
